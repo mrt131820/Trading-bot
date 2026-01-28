03aa9229ed53e3d891b83adbf0c9cc38e29c3aac
Mritunjaya Tripathi <mritunjaya.tripathi@pagarbook.com>
2026-01-28 18:54:34 +0530

    Removing secrets

--- src/credentials_angel_one.py
+++ src/credentials_angel_one.py
@@ -1,8 +1,8 @@
 import os
 
 credentials = {
-  "api_key": os.getenv("API_KEY", "q2oK1kt9"),
-  "client_code": os.getenv("CLIENT_CODE", "M1044520"),
-  "password": os.getenv("PASSWORD", "2712"),
-  "totp_secret": os.getenv("TOTP_SECRET", "WOSGISA3BB3OIW5KR732NBZXZI")
+  "api_key": os.getenv("API_KEY"),
+  "client_code": os.getenv("CLIENT_CODE"),
+  "password": os.getenv("PASSWORD"),
+  "totp_secret": os.getenv("TOTP_SECRET")
 }
